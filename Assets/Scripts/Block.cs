8b1c1fb5c7b21a8b5cd5aa4544de2647e0cc0c48
Jessica Phuong <jessicaphuong02@gmail.com>
2015-05-01 19:51:51 +0930

    Submit current version of the arkanoid clone

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D collisionInfo) {
+		// Destroy the whole Block
+		// Destroy(this) would only destroy the Block Script component. 
+		// If we want to destroy the whole Block, 
+		// then we always have to use Destroy(gameObject)
+		Destroy(gameObject);
+	}
+}
